f1a8b4fad899e1b0ffbf3f6919074c5c1138f52e
anjit pariyar <anjitpariyar@gmail.com>
2025-05-19 01:46:51 +0545

    fix: footer center

--- components/layouts/footer.tsx
+++ components/layouts/footer.tsx
@@ -4,7 +4,7 @@ import React from 'react'
 function Footer() {
   return (
     <footer className='bg-background w-full border-t py-6'>
-      <div className='mx-absolute container px-4 md:px-6'>
+      <div className='container mx-auto px-4 md:px-6'>
         <div className='flex flex-col items-center justify-center gap-4 text-center'>
           <div className='flex items-center gap-2'>
             <BookOpen className='text-primary h-6 w-6' />
